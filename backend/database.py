426a4181b03d6058026736ac58c57c43e8d24e07
Khushpreet Kaur <khushpreet@gmail.com>
2026-04-28 17:54:46 +0530

    fix mysql env config

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,23 @@
+# import mysql.connector
+
+# conn = mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     password="KidanFer22G@",
+#     database="smart_task_db"
+# )
+
+# cursor = conn.cursor(dictionary=True)
+
+
+import os
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="KidanFer22G@",
-    database="smart_task_db"
+    host=os.getenv("DB_HOST", "localhost"),
+    user=os.getenv("DB_USER", "root"),
+    password=os.getenv("DB_PASSWORD", "KidanFer22G@"),
+    database=os.getenv("DB_NAME", "smart_task_db")
 )
 
-cursor = conn.cursor(dictionary=True)
+cursor = conn.cursor()
